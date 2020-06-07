2167942830f7015b8eb6fe5657cf308e663fbed5
Angelika Postaremczak <angelikapostar@gmail.com>
2020-06-07 03:27:48 +0200

    User FixedLenSequenceFeature and fix parsing data from tf record files

--- utils/comment_tf_dataset.py
+++ utils/comment_tf_dataset.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 
-from constants import MAX_SEQ_LENGTH, NUM_CLASSES
+from utils.constants import MAX_SEQ_LENGTH, NUM_CLASSES
 
 FEATURE_DESCRIPTION = {
-    'input_token': tf.io.FixedLenFeature([MAX_SEQ_LENGTH], tf.int64),
-    'masked_token': tf.io.FixedLenFeature([MAX_SEQ_LENGTH], tf.int64),
-    'label': tf.io.FixedLenFeature([NUM_CLASSES], tf.float32),
+    'input_token': tf.io.FixedLenSequenceFeature([MAX_SEQ_LENGTH], tf.int64,
+                                                 allow_missing=True),
+    'masked_token': tf.io.FixedLenSequenceFeature([MAX_SEQ_LENGTH], tf.int64,
+                                                  allow_missing=True),
+    'label': tf.io.FixedLenSequenceFeature([NUM_CLASSES], tf.float32,
+                                           allow_missing=True)
 }
 
 
@@ -17,10 +20,9 @@ def _parse_comment_record(serialized_example):
     mask = features["masked_token"]
     label = features["label"]
 
-    return input_token, mask, label
+    return (input_token, mask), label
 
 
 def load_comment_dataset(tf_record_file_path: str):
     raw_dataset = tf.data.TFRecordDataset(tf_record_file_path)
     return raw_dataset.map(_parse_comment_record)
-
